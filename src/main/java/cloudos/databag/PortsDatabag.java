c8494a5cced6e861ad897b56c2f46b387e7e682c
jonathan <jonathan@kyuss.org>
2014-10-23 21:23:31 -0700

    add admin port

--- src/main/java/cloudos/databag/PortsDatabag.java
+++ src/main/java/cloudos/databag/PortsDatabag.java
@@ -1,14 +1,18 @@
 package cloudos.databag;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 
+@NoArgsConstructor @Accessors(chain=true)
 public class PortsDatabag {
 
     public String getId() { return "ports"; }
     public void setId (String id) { /*noop*/ }
 
     @Getter @Setter private int primary;
+    @Getter @Setter private int admin;
 
     public PortsDatabag (int port) { this.primary = port; }
 
