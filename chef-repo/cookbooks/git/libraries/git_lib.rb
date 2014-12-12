a283131aa08c8085d895b6de3dc5d65969711782
jonathan <jonathan@kyuss.org>
2014-12-12 11:56:58 -0800

    git library utils should not assume the run_as user can login. use sudo instead.

--- chef-repo/cookbooks/git/libraries/git_lib.rb
+++ chef-repo/cookbooks/git/libraries/git_lib.rb
@@ -6,10 +6,10 @@ class Chef::Recipe::Git
     github_hostkey = chef.node[:git][:github_hostkey]
     known_hosts = "~#{run_as}/#{KNOWN_HOSTS}"
     chef.bash "set github known host key for #{run_as}" do
-      user run_as
+      user 'root'
       code <<-EOF
-echo '#{github_hostkey}' >> #{known_hosts}
-chmod 644 #{known_hosts}
+sudo -u #{run_as} -H bash -c "echo '#{github_hostkey}' >> #{known_hosts}"
+sudo -u #{run_as} -H chmod 644 #{known_hosts}
       EOF
       not_if { File.exists?(known_hosts) && `cat #{known_hosts}`.lines.grep(github_hostkey).size > 0 }
     end
@@ -18,10 +18,10 @@ chmod 644 #{known_hosts}
   def self.clone (chef, repo, branch, run_as, cwd, to_dir = nil)
     dir = to_dir || base_name(repo)
     chef.bash "clone repository #{repo} on branch #{branch} into dir #{dir}" do
-      user run_as
+      user 'root'
       cwd cwd
       code <<-EOF
-git clone #{repo} --branch #{branch} #{dir}
+sudo -u #{run_as} -H git clone #{repo} --branch #{branch} #{dir}
       EOF
       not_if { File.exists? "#{cwd}/#{dir}"  }
     end
