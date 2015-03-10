29af1cdfbd7da1be0ff996fc482e27032cb404e2
jonathan <jonathan@kyuss.org>
2015-03-09 20:10:33 -0700

    add AdminDatabag

---
+++ src/main/java/cloudos/databag/AdminDatabag.java
@@ -0,0 +1,19 @@
+package cloudos.databag;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor @AllArgsConstructor
+public class AdminDatabag {
+
+    public static final String ID = "admin";
+
+    public String getId() { return ID; }
+    public void setId (String id) { /*noop*/ }
+
+    @Getter @Setter private String name;
+    @Getter @Setter private String email;
+
+}
