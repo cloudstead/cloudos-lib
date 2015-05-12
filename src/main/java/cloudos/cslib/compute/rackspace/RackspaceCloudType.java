ce468ebf473a56467cf20fb2d3590fa96826e2af
jonathan <jonathan@kyuss.org>
2015-05-12 12:54:57 -0700

    fix rackspace constants

--- src/main/java/cloudos/cslib/compute/rackspace/RackspaceCloudType.java
+++ src/main/java/cloudos/cslib/compute/rackspace/RackspaceCloudType.java
@@ -6,7 +6,7 @@ public class RackspaceCloudType extends CsCloudTypeBase<RackspaceCloud> {
 
     public static final RackspaceCloudType TYPE = new RackspaceCloudType();
 
-    @Override public String getProviderName() { return "openstack-nova"; }
+    @Override public String getProviderName() { return "rackspace-cloudservers-us"; }
 
     @Override protected String getRegionsJson() { return "cloudos/cslib/rs/regions.json"; }
     @Override protected String getInstanceTypesJson() { return "cloudos/cslib/rs/instance_types.json"; }
