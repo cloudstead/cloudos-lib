5217f6f2ba0a5e219ed6d991570b354fa6237c97
jonathan <jonathan@kyuss.org>
2015-06-05 15:10:29 -0700

    simplify

--- src/main/java/cloudos/cslib/compute/meta/CsFootprint.java
+++ src/main/java/cloudos/cslib/compute/meta/CsFootprint.java
@@ -6,10 +6,12 @@ import lombok.Setter;
 import lombok.experimental.Accessors;
 import org.cobbzilla.util.reflect.ReflectionUtil;
 
+import static org.cobbzilla.util.reflect.ReflectionUtil.copy;
+
 @NoArgsConstructor @Accessors(chain=true)
 public class CsFootprint implements Footprint {
 
-    public CsFootprint(CsFootprint other) { ReflectionUtil.copy(this, other); }
+    public CsFootprint(CsFootprint other) { copy(this, other); }
 
     @Getter @Setter private Integer cpus;
     @Getter @Setter private Integer memory;
