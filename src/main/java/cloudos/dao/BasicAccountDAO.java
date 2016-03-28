caf09b2c29d04ded609c94af83447b07a2ccbe89
jonathan <jonathan@kyuss.org>
2016-03-28 11:37:18 -0700

    remove public modifiers from interface

--- src/main/java/cloudos/dao/BasicAccountDAO.java
+++ src/main/java/cloudos/dao/BasicAccountDAO.java
@@ -7,13 +7,13 @@ import org.cobbzilla.wizard.dao.DAO;
 
 public interface BasicAccountDAO<A extends BasicAccount> extends DAO<A> {
 
-    public A findByName(String name);
+    A findByName(String name);
 
-    public A authenticate(LoginRequest login) throws AuthenticationException;
+    A authenticate(LoginRequest login) throws AuthenticationException;
 
-    public A findByActivationKey(String key);
+    A findByActivationKey(String key);
 
-    public A findByResetPasswordToken(String token);
+    A findByResetPasswordToken(String token);
 
-    public void setPassword(A account, String password);
+    void setPassword(A account, String password);
 }
