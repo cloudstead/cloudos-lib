b18895e31c23eb3c7aa6d4976c16e3d4f9abcc4c
jonathan <jonathan@kyuss.org>
2016-10-31 17:33:56 -0700

    use HttpContentTypes.APPLICATION_JSON constant

--- src/main/java/cloudos/service/asset/AssetStorageService.java
+++ src/main/java/cloudos/service/asset/AssetStorageService.java
@@ -3,12 +3,14 @@ package cloudos.service.asset;
 import cloudos.server.asset.AssetStorageConfiguration;
 import cloudos.server.asset.AssetStorageType;
 import com.amazonaws.services.s3.internal.Mimetypes;
+import org.cobbzilla.util.http.HttpContentTypes;
 import org.cobbzilla.util.io.FileUtil;
 
 import java.io.File;
 import java.io.InputStream;
 
 import static org.cobbzilla.util.daemon.ZillaRuntime.die;
+import static org.cobbzilla.util.http.HttpContentTypes.APPLICATION_JSON;
 import static org.cobbzilla.util.security.ShaUtil.sha256_file;
 
 public abstract class AssetStorageService {
@@ -41,8 +43,6 @@ public abstract class AssetStorageService {
     public String getUri(String sha256, String filename) {
         return sha256.substring(0, 2) + "/" + sha256.substring(2, 4) + "/" + sha256.substring(4, 6) + "/" + sha256.substring(6, 8) + "/" + sha256 + FileUtil.extension(filename);
     }
-
-    public static final String APPLICATION_JSON = "application/json";
 
     public String getMimeType(String filename) {
         return filename.endsWith(".json") ? APPLICATION_JSON : Mimetypes.getInstance().getMimetype(filename);
